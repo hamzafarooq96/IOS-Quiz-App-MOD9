3b9f8739b41984255494b100b85e5d6a258eb5ae
Hamza Farooq <51108114+hamzafarooq96@users.noreply.github.com>
2020-06-04 13:49:40 +0500

    Committed
    Commit Code

---
+++ Quizzler-iOS13/Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,22 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by IOS Team on 03/06/2020.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text :String
+    let answer : String
+    
+    init(q: String, a: String) {
+       
+      text = q
+        
+        answer = a
+        
+    }
+}
